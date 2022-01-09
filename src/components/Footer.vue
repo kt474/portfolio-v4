a07d31461d49f89b0dacf9aa98a8a392599f13d9
Kevin Tian <kevin.tian@ibm.com>
2022-01-09 15:43:53 -0500

    setup initial layout

---
+++ src/components/Footer.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { ref } from "vue";
+</script>
+
+<template>
+  <div class="text-white absolute bottom-16 ml-8">
+    <p class="text-2xl uppercase tracking-widest">Email</p>
+    <a href="mailto:kt474@cornell.edu" class="text-xl">kt474@cornell.edu</a>
+    <p class="text-2xl uppercase tracking-widest mt-4">Social</p>
+    <div class="flex justify-between mt-2">
+      <a href="https://www.linkedin.com/in/kt474/" target="_blank"
+        ><img
+          src="../assets/linkedin_icon.svg"
+          alt=""
+          width="24"
+          height="24"
+          title="Linkedin"
+      /></a>
+      <a href="https://github.com/kt474" target="_blank"
+        ><img
+          src="../assets/github_icon.svg"
+          alt=""
+          width="24"
+          height="24"
+          title="Github"
+      /></a>
+      <a href="https://www.instagram.com/kevkevkt/" target="_blank">
+        <img
+          src="../assets/instagram_icon.svg"
+          alt=""
+          width="24"
+          height="24"
+          title="Instagram"
+        />
+      </a>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Main.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Footer from "./Footer.vue";
+import { ref } from "vue";
+</script>
+
+<template>
+  <div class="w-full flex">
+    <div class="basis-5/12 bg-rose-700 h-screen">
+      <Footer />
+    </div>
+    <img
+      class="absolute max-w-1/4 profile-img"
+      src="../assets/profile_pic.jpeg"
+      height="400"
+    />
+    <div class="basis-7/12 h-screen"></div>
+  </div>
+</template>
+
+<style scoped>
+.profile-img {
+  margin-left: 29%;
+  margin-top: 16%;
+}
+</style>
