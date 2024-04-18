77313d54a45a113ad6f1f54e9cc0b4a3fe4d397e
kt474 <kt474@cornell.edu>
2024-04-18 00:13:48 -0400

    add overscroll background

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,29 @@
 <script setup lang="ts">
 import Main from "./components/Main.vue";
+// @ts-ignore
+import { useStore } from "./store/store.js";
+import { onMounted, watch } from "vue";
+const store = useStore();
+
+onMounted(() => {
+  setHtmlBackground();
+});
+
+watch(
+  () => store.darkMode,
+  () => setHtmlBackground()
+);
+
+const setHtmlBackground = () => {
+  const html = document.querySelector("html");
+  if (html) {
+    if (store.darkMode) {
+      html.style.background = "#1e1e1e";
+    } else {
+      html.style.background = "#f1f5f9";
+    }
+  }
+};
 </script>
 
 <template>
