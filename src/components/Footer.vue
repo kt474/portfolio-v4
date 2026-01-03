5d63f851451160a8f71ffb88380002c676550131
kt474 <kt474@cornell.edu>
2026-01-03 14:15:33 -0500

    Revert "Redesign portfoflio"
    This reverts commit 0f19d7d1169015a05e7dc3be07e29118b96d40ce.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,10 +14,10 @@ const store = useStore();
     </div>
     <div class="m-0 mt-6 md:m-2">
       <p class="font-bold tracking-wide text-md md:text-2xl">Socials</p>
-      <div class="flex justify-start w-40 my-1">
+      <div class="flex justify-start w-40 my-1 sm:justify-between">
         <a
           aria-label="Linkedin"
-          class="pr-1 mr-4 sm:mr-8"
+          class="pr-1"
           href="https://www.linkedin.com/in/kt474/"
           target="_blank"
           ><img
@@ -57,6 +57,29 @@ const store = useStore();
             height="24"
             title="Github"
         /></a>
+        <a
+          aria-label="Twitter"
+          class="px-1 -mt-0.5"
+          href="https://twitter.com/kevkevkt"
+          target="_blank"
+        >
+          <img
+            v-if="store.darkMode"
+            src="../assets/twitterx_dark.svg"
+            alt=""
+            width="28"
+            height="28"
+            title="Twitter"
+          />
+          <img
+            v-else
+            src="../assets/twitterx.svg"
+            alt=""
+            width="28"
+            height="28"
+            title="Twitter"
+          />
+        </a>
       </div>
     </div>
   </div>
